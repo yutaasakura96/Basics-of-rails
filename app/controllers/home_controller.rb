2c52ef883ed6e25934e403c96a01a5bddc81fd39
Yuta Asakura <yuta.asakura.se@gmail.com>
2024-06-26 18:53:52 +0900

    Working with built-in helper methods

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -48,4 +48,27 @@ class HomeController < ApplicationController
   def showdatetimeinfo
     @currenttime=DateTime.now
   end
+
+  def conditionalrendering
+    @data={
+      'name':'Jimmy Anderson',
+      'isVisible':true,
+      'loggedIn':false,
+      'countryCode':'US',
+      'workExperience':45,
+      'stateCode':nil
+    }
+  end
+
+  def loopthrougharrays
+    @Products = []
+    @Products.push( {'productID':1,'productName':"AMD Ryzen 3990",'quantity':100,'unitInStock':50,'disContinued':'False','cost':3000})
+    @Products.push( {'productID':2,'productName':"AMD Ryzen 3980",'quantity':100,'unitInStock':50,'disContinued':'False','cost':4000})
+    @Products.push( {'productID':3,'productName':"AMD Ryzen 3970",'quantity':100,'unitInStock':50,'disContinued':'False','cost':5000})
+    @Products.push( {'productID':4,'productName':"AMD Ryzen 3960",'quantity':100,'unitInStock':50,'disContinued':'False','cost':6000})
+    @Products.push( {'productID':5,'productName':"AMD Ryzen 3950",'quantity':100,'unitInStock':50,'disContinued':'False','cost':7000})
+    @Products.push( {'productID':6,'productName':"AMD Ryzen 3940",'quantity':100,'unitInStock':50,'disContinued':'True','cost':8000})
+    @Products.push( {'productID':7,'productName':"AMD Ryzen 3930",'quantity':100,'unitInStock':50,'disContinued':'True','cost':9000})
+    @Products.push( {'productID':8,'productName':"AMD Ryzen 3920",'quantity':100,'unitInStock':50,'disContinued':'True','cost':10000})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,6 @@ Rails.application.routes.draw do
   get "home/getquerystringvalues" => "home#getquerystringvalues"
   get "home/passdatatoviews" => "home#passdatatoviews"
   get "home/showdatetimeinfo" => "home#showdatetimeinfo"
+  get "home/conditionalrendering" => "home#conditionalrendering"
+  get "home/loopthrougharrays" => "home#loopthrougharrays"
 end
